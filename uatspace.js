1b2fc0905d9683525694d9a814c225fd72005bc8
Trinity <shaky_dlo_680@hotmail.com>
2021-12-15 11:08:53 -0600

    Add files via upload

---
+++ uatspace.js
@@ -0,0 +1,9 @@
+function programStart(){
+    document.getElementById("start").disabled = true;
+    document.getElementById("stop").disabled = false;
+}
+
+function programStop(){
+    document.getElementById("stop").disabled = true;
+    document.getElementById("start").disabled = false;
+}
